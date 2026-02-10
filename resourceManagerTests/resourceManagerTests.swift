dedcbca23232853219dea3610f6b7cd3d04c3408
Atharva Sonawane <atharva10@Atharvas-MacBook-Air.local>
2026-02-10 15:58:06 -0500

    first commit

---
+++ resourceManagerTests/resourceManagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  resourceManagerTests.swift
+//  resourceManagerTests
+//
+//  Created by Atharva Sonawane on 2/10/26.
+//
+
+import Testing
+@testable import resourceManager
+
+struct resourceManagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
